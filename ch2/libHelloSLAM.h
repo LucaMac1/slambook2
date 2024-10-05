0f2673733e1810d3469d8b9b99c2c0245058e342
LucaMac1 <macchiusiluca@gmail.com>
2024-10-05 18:35:38 +0200

    Add ch2 and ch3

---
+++ ch2/libHelloSLAM.h
@@ -0,0 +1,9 @@
+#ifndef LIBHELLOSLAM_H_
+#define LIBHELLOSLAM_H_
+// The above macro definition is to prevent redefinition errors
+// caused by duplicate inclusion of this header file.
+
+// Function to print a sentence "hello".
+void printHello();
+
+#endif
